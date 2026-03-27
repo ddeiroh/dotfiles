6cc54ac2b8a7418b47a7941af89a084c7c6e0f3e
Devison Deiró <devison.jesus@saude.ba.gov.br>
2026-03-27 17:22:04 -0300

    restructure files in the repo

--- altskel/scripts/install_cht.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Installation of the unified comunity driven cheat sheets - cht.sh
-
-PATH_DIR="$HOME/bin"
-mkdir -p "$PATH_DIR"
-curl https://cht.sh/:cht.sh > "$PATH_DIR/cht.sh"
-chmod +x "$PATH_DIR/cht.sh"
